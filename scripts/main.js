04a529b27239259054b70cbd51373d2ae0fe0efb
Strassnix <hans.hubertus3000@gmail.com>
2018-06-13 14:48:19 +0200

    created json for data & implementing vue

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,23 @@
-/*$("#back").on("click", function(){
-    window.history.back();
-})*/
+var app = new Vue({
+    el: ".match",
+    data: {
+        json: "scripts/data.json",
+        teams: [],
+        locations: [],
 
+    },
+    created: function() {
+        this.getData();
+    },
+    methods: {
+        getData: function () {
+            $.getJSON(this.json, function (data) {
+                app.teams = data.teams;
+                app.locations = data.locations;
+                console.log(app.locations);
+            })
+        },
+
+
+    }
+})
