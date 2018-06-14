a40e74ca9d43caf5a0562374c502d246deec2a3f
Strassnix <hans.hubertus3000@gmail.com>
2018-06-14 14:46:34 +0200

    changing html and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,49 @@ var app = new Vue({
         json: "scripts/data.json",
         teams: [],
         locations: [],
+        games: [],
+        allGames: [],
+        hide: true
 
     },
-    created: function() {
+    created: function () {
         this.getData();
+
     },
     methods: {
         getData: function () {
             $.getJSON(this.json, function (data) {
                 app.teams = data.teams;
                 app.locations = data.locations;
-                console.log(app.locations);
+                app.games = data.games;
+                app.allGames = data.games;
             })
         },
+        openLocations: function (x) {
 
+            window.open(x);
+
+        },
+        openTeam: function (x) {
+
+            /*window.location.href = "detailedTeams.html"*/
+            
+            this.games = this.allGames;
+            var teamName = x.getAttribute("data-value");
+            
+            console.log(teamName);
+            
+            var gamesToShow = this.games.filter(function(game){
+                return game.teams.split(" ").includes(teamName);
+            })
+            
+            this.games = gamesToShow;    
+            
+            /*$(".buttons").hide();*/
+            
+            this.hide = false;
+            
+        }
 
     }
 })
